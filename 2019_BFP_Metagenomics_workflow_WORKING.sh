e201af5f97075c3c63bf8722e5fe5eb6bc128bfe
ChrisTrivedi <13127554+ChrisTrivedi@users.noreply.github.com>
2020-01-25 15:45:47 +0100

    Delete 2019_BFP_Metagenomics_workflow_WORKING.sh

--- 2019_BFP_Metagenomics_workflow_WORKING.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-### Description ###
-
-This is the updated Metagenomics commands for the upcoming publication of Trivedi, et al. Let this server as a placeholder until all commands and comments are finalized.
